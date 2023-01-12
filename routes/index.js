f3e27258b7862994e6cc23178389925f9805bec7
Liquid Monks <info@liquidmonks.com>
2023-01-12 07:40:22 -0600

    Project directory structure includes a routes and helpers directory that contains the appropriate JS variable declarations and routing path requirements.

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+// Import our modular routers for /tips and /feedback
+const tipsRouter = require('./tips');
+const feedbackRouter = require('./feedback');
+const diagnosticsRouter = require('./diagnostics');
+
+const app = express();
+
+app.use('/notes',notesRouter);
+
+
+
+module.exports = app;
